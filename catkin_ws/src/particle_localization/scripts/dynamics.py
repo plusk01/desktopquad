68d7ebd7c520bba289b2c7ec524024e894880a71
Timothy Devon Morris <devonmorris1992@byu.edu>
2017-11-10 16:14:32 -0700

    fixed dynamics according to small unmanned aircraft - Beard

--- catkin_ws/src/particle_localization/scripts/dynamics.py
+++ catkin_ws/src/particle_localization/scripts/dynamics.py
@@ -9,14 +9,14 @@ class ConstVel(object):
     thus, all state-estimates will be maintained outside of the class
     """
 
-    def __init__(self, M_acc, M_gyro):
+    def __init__(self, Q_pos, Q_att):
         """
         Inputs:
         M_acc - 3x3 np array input noise
         M_gyro - 3x3 np array input noise
         """
-        self.M_acc = M_acc
-        self.M_gyro = M_gyro
+        self.Q_pos = Q_pos
+        self.Q_att = Q_att
 
     def propogate(self, X, u, dt):
         """
@@ -31,15 +31,15 @@ class ConstVel(object):
         Outputs:
         Xp - predictions  
         """
-        k1 = self.dynamics(X, u)
-        k2 = self.dynamics(X + dt*k1/2, u) 
-        k3 = self.dynamics(X + dt*k2/2, u)
-        k4 = self.dynamics(X + dt*k3, u)
+        k1 = self.dynamics(X, u, dt)
+        k2 = self.dynamics(X + dt*k1/2, u, dt) 
+        k3 = self.dynamics(X + dt*k2/2, u, dt)
+        k4 = self.dynamics(X + dt*k3, u, dt)
 
         Xp = X + (dt/6)*(k1 + 2*k2 + 2*k3 + k4)
         return Xp
 
-    def dynamics(self, X, u):
+    def dynamics(self, X, u, dt):
         """
         Calculate the derivative of the state
         Inputs:
@@ -49,19 +49,57 @@ class ConstVel(object):
         Outputs:
         Xdot - derivative of the states
         """
-        sh = X.shape[0]
+        g = 9.8
         acc = u[:3]
         gyro = u[3:]
-        Xdot = np.zeros(sh)
-        Xdot[:3] = X[6:9]
-        Xdot[3:6] = gyro + np.random.multivariate_normal(mean=mu, cov=self.M_gyro)
+
+        sh = X.shape
+        cphi = np.cos(X[6])
+        sphi = np.sin(X[6])
+        cth = np.cos(X[7])
+        sth = np.cos(X[7])
+        cpsi = np.cos(X[8])
+        spsi = np.cos(X[8])
+
+        u = X[3]
+        v = X[4]
+        w = X[5]
+
+        p = X[9]
+        q = X[10]
+        r = X[11]
+
+        A = np.zeros((3,3,sh[1]))
+        A[0,0] = cth*cpsi
+        A[0,1] = sphi*sth*cpsi - cphi*spsi
+        A[0,2] = cphi*sth*cpsi + sphi*spsi
+        A[1,0] = cth*spsi
+        A[1,1] = sphi*sth*spsi + cth*cpsi
+        A[1,2] = cphi*sth*spsi - sphi*cpsi
+        A[2,0] = -sth
+        A[2,1] = sphi*cth
+        A[2,2] = cphi*cth
+
+        B = np.zeros((3,3,sh[1]))
+        B[0,0] = 1
+        B[0,1] = sphi*sth/cth
+        B[0,2] = cphi*sth/cth
+        B[1,1] = cphi
+        B[1,2] = -sphi
+        B[2,1] = sphi/cth
+        B[2,2] = cphi/cth
 
         mu = np.zeros(3)
-        # Does it make sense to add in noise during the integration?
-        # Or should this be added on in propogate?
-        Xdot[6:9] = acc + np.random.multivariate_normal(mean=mu, cov=self.M_acc) 
+        Xdot = np.zeros(sh)
+        Xdot[:3] = np.einsum('ijk,jk->ik',A,X[3:6])
+        Xdot[3] = acc[0] + q*w - r*v + g*sth
+        Xdot[4] = acc[1] + r*u - p*w - g*cth*sphi
+        Xdot[5] = acc[2] + p*v - q*u - g*cth*cphi
+        Xdot[6:9] = np.einsum('ijk,jk->ik',B,X[9:12])
+        Xdot[9:] = (gyro)[:,np.newaxis]
 
         return Xdot
+
 
 
 class QuadRotor(object):
--- catkin_ws/src/particle_localization/scripts/particle_localization.py
+++ catkin_ws/src/particle_localization/scripts/particle_localization.py
@@ -2,9 +2,11 @@
 import rospy
 import time, tf
 import numpy as np
-from sensor_msgs.imu import Imu
+from sensor_msgs.msg import Imu
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Vector3Stamped
 from dynamics import ConstVel as Dynam
-from aruco_localization import MarkerMeasurement, MarkerMeasurementArray
+from aruco_localization.msg import MarkerMeasurement, MarkerMeasurementArray
 
 class ParticleLocalization:
     """
@@ -21,69 +23,116 @@ class ParticleLocalization:
             rospy.logfatal('Params not set in /desktopquad namespace')
             rospy.signal_shutdown('Params not set')
 
-        self.map = self.param['map']
-        M_acc = self.param['M_acc']
-        M_gyro = self.param['M_gyro']
-        self.R = self.param['R_aruco']
+        self.map = self.param['landmarks']
+        #M_acc = self.param['M_acc']
+        #M_gyro = self.param['M_gyro']
+        #self.R = self.param['R_aruco']
+        Q_pos = np.diag(3*[1e-3])
+        Q_att = np.diag(3*[1e-3])
 
-        n_particles = self.param['n_particles']
+        #n_particles = self.param['n_particles']
+        n_particles = 1
 
         # PARKER ARUCO STUFF
-        rospy.Subscriber("measurements", MarkerMeasurementArray, self.aruco_callback)
+        rospy.Subscriber("chiny/aruco/measurements", MarkerMeasurementArray, self.aruco_callback)
         # ROSFLIGHT ACCEL & GYRO STUFF
-        rospy.Subscriber("imu/data", Imu, self.imu_callback)
+        rospy.Subscriber("chiny/imu/data", Imu, self.imu_callback)
+        rospy.Subscriber("chiny/imu/acc_bias", Vector3Stamped, self.imu_acc_callback)
+        rospy.Subscriber("chiny/imu/gyro_bias", Vector3Stamped, self.imu_gyro_callback)
+        self.tmp_pub_ = rospy.Publisher("tmp/", Odometry)
 
         self.init_particles(n_particles)
 
-        self.dynamics = Dynam(M_acc, M_gyro)
+        self.acc_bias = np.zeros(3)
+        self.gyro_bias = np.zeros(3)
+        self.dynamics = Dynam(Q_pos, Q_att)
         self.particle_update = False
+        self.particle_propogate = False
         self.prev_time = rospy.Time.now()
         self.measurements = []
         self.init_particles(n_particles)
         self.run()
 
     def init_particles(self, n_particles):
-        # need to think about how to efficiently initialize particles
+        """
+        These particles are stored in columns representing
+        positions, velocities, attitude and attitude rates
+        as described below.
+
+        0 - p_n intertial north
+        1 - p_e inertial east
+        2 - p_d intertial down
+        3 - u body frame velocity along i^b
+        4 - v body frame velocity along j^b
+        5 - body frame velocity along k^b
+        6 - phi roll angle
+        7 - theta pitch angle
+        8 - yaw angle
+        9 - p roll rate along i^b
+        10 - q pitch rate along i^b
+        11 - r yaw rate along k^b
+        """
         mins = self.param['mins']
         maxs = self.param['maxs']
         self.particles = np.zeros((12, n_particles))
-        for i in xrange(len(mins)):
-            self.particles[i] = np.random.uniform(low=mins[i],
-                                high=maxs[i], size=n_particles)
-
-        self.particles[6:] = np.random.randn(3,n_particles)
-
-    def propagate(self, dt):
-        # call dynamic model on particles
-        self.particles = dynam.propogate(self.particles, self.imu, dt)
+        for i in xrange(len(mins)//2):
+            self.particles[i] = np.random.uniform(low=float(mins[i]),
+                                high=float(maxs[i]), size=n_particles)
+            self.particles[6+i] = np.random.uniform(low=float(mins[3+i]),
+                                high=float(maxs[3+i]), size = n_particles)
 
     def localize(self, msg):
-        for aruco_m in msg:
-            sig = aruco_m.aruco_id
+        """
+        """
+        pass
 
     def run(self):
         rate = rospy.Rate(20)
         while not rospy.is_shutdown():
-            self.localize()
             rate.sleep()
 
-    def aruco_callback(self, msg)
-        # coordinate w/ parker on this one
+    def aruco_callback(self, msg):
+        """
+        """
         self.particle_update = True
         self.localize(msg)
         self.particle_update = False
 
-    def imu_callback(self, msg)
-        # store accel & gyro for dynamics
-        u = np.zeros(6)
-        u[0:3] = msg.linear_acceleration
-        u[4:] = msg.angular_velocity
-        if not self.particle_update
-            dt = self.prev_time - rospy.Time.now()
-            self.prev_time = rospy.Time.now()
-            self.particles = self.dynamics.propogate(
-                                self.particles,u, dt)
+    def imu_callback(self, msg):
+        """
+        """
+        if not self.particle_update :
+            dt = (rospy.Time.now() - self.prev_time).to_sec()
+            if dt > .1:
+                self.prev_time = rospy.Time.now()
+                # store accel & gyro for dynamics
+                u = np.zeros(6)
+                u[0:3] = [msg.linear_acceleration.x,msg.linear_acceleration.y, -msg.linear_acceleration.z]
+                #u[3:] = [msg.angular_velocity.x,msg.angular_velocity.y,msg.angular_velocity.z]
 
+                # correct for imu bias
+                u[0:3] -= self.acc_bias
+                #u[3:] -= self.gyro_bias
+
+                # update particles
+                self.particles = self.dynamics.propogate(
+                                self.particles, u, dt)
+
+                x_hat = np.mean(self.particles, axis=1) 
+                print x_hat
+                o_msg = Odometry()
+                o_msg.header.frame_id = "world"
+                o_msg.pose.pose.position.x = x_hat[0]
+                o_msg.pose.pose.position.y = x_hat[1]
+                o_msg.pose.pose.position.z = x_hat[2]
+                o_msg.pose.pose.orientation.x = 1.
+                self.tmp_pub_.publish(o_msg)
+
+    def imu_acc_callback(self, msg):
+        self.acc_bias[:] = [msg.vector.x, msg.vector.y, msg.vector.z]
+
+    def imu_gyro_callback(self, msg):
+        self.gyro_bias[:] = [msg.vector.x, msg.vector.y, msg.vector.z]
 
 def main():
     # initialize node
@@ -91,3 +140,6 @@ def main():
 
     pf = ParticleLocalization()
     pf.run()
+
+if __name__=="__main__":
+    main()
